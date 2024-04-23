755207ffb11419cacc91226817e17d52e2e5cf11
Leann Abellanosa <labellanosa@luc.edu>
2024-04-23 05:07:18 +0000

    updated character

--- src/character.c
+++ src/character.c
@@ -6,12 +6,14 @@ Attributes createStudent(int bravery, int intellect, int loyalty, int spellCasti
 }
 
 void updateAttributes(Attributes *student, int bravery, int intellect, int loyalty) {
-    student->bravery += bravery;
-    student->intellect += intellect;
-    student->loyalty += loyalty;
+    student->bravery = (student->bravery + bravery >= 0) ? student->bravery + bravery : 0;
+    student->intellect = (student->intellect + intellect >= 0) ? student->intellect + intellect : 0;
+    student->loyalty = (student->loyalty + loyalty >= 0) ? student->loyalty + loyalty : 0;
 }
 
 void updateSkills(Attributes *student, int spellCasting, int potionMaking, int flying) {
-    student->spellCasting += spellCasting;
-    student->potionMaking += potionMaking;
-    student->flying += flying;
+    student->spellCasting = (student->spellCasting + spellCasting >= 0) ? student->spellCasting + spellCasting : 0;
+    student->potionMaking = (student->potionMaking + potionMaking >= 0) ? student->potionMaking + potionMaking : 0;
+    student->flying = (student->flying + flying >= 0) ? student->flying + flying : 0;
+}
+
