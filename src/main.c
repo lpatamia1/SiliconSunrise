19a7e6d2de97cfa06414f72151baf046702c86a0
Leann Abellanosa <labellanosa@luc.edu>
2024-04-23 03:42:44 +0000

    made changes to implement player

--- src/main.c
+++ src/main.c
@@ -6,6 +6,6 @@
 int main() {
     HogwartsStudent player = createStudent(5, 5, 5, 1, 1, 1);
     Node* startNode = setupGame();
-    playGame(startNode);
+    playGame(startNode, &player);
     return 0;
 }
